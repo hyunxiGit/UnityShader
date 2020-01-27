a732f617ac01fd6f83f90530fa65180b4a276522
hyunxi <hyunxi@hotmail.com>
2020-01-26 17:55:26 -0800

    add compute shader basic structure

---
+++ script/compute_shader_connect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compute_shader_connect : MonoBehaviour
+{
+	public ComputeShader compute;
+
+	public RenderTexture result;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	int kernel = compute.FindKernel("CSMain");
+    	result = new RenderTexture(512,512,24);
+        result.enableRandomWrite = true;
+        result.Create();
+
+        compute.SetTexture(kernel, "Result" , result);
+        compute.Dispatch(kernel, 512/8, 512/8, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
