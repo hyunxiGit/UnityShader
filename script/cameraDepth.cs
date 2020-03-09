a19c77349b4795545463bd06d3e11a334cc2eee6
hyunxi <hyunxi@hotmail.com>
2020-03-08 17:07:53 -0700

    add ability to render depth to static png texture

--- script/cameraDepth.cs
+++ script/cameraDepth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 [ExecuteInEditMode]
 public class cameraDepth : MonoBehaviour
 {
@@ -27,12 +28,23 @@ public class cameraDepth : MonoBehaviour
          }
     }
     private Camera camera;
+    private Texture2D depth_texture;
+    private bool write_once = false;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(shader);
         Debug.Log(material);
         camera = GetComponent<Camera>();
+        int w =128;
+        int h =128;
+        if (camera != null)
+        {
+        	w = camera.pixelWidth;
+        	h = camera.pixelHeight;
+        }
+
+        Debug.Log("camera res : " + camera.pixelWidth);
  		if (!SystemInfo.supportsImageEffects)
 		{
 			Debug.Log("System doesn't support image effects");
@@ -47,7 +59,9 @@ public class cameraDepth : MonoBehaviour
 		}
 
 		// turn on depth rendering for the camera so that the shader can access it via _CameraDepthTexture
+		//https://docs.unity3d.com/Manual/SL-CameraDepthTexture.html
 		camera.depthTextureMode = DepthTextureMode.Depth;
+
     }
 
 	private void OnDisable()
@@ -62,6 +76,20 @@ public class cameraDepth : MonoBehaviour
 		{
 			material.SetFloat("_DepthLevel", depthLevel);
 			Graphics.Blit(src, dest, material);
+
+			if (!write_once)
+			{
+				depth_texture = new Texture2D(src.width, src.height, TextureFormat.RGB24, false);
+				depth_texture.ReadPixels(new Rect(0, 0, src.width, src.height), 0, 0);
+	 			depth_texture.Apply();
+
+	 			byte[] bytes;
+	     		bytes = depth_texture.EncodeToPNG();
+	     		System.IO.File.WriteAllBytes( "d:my_depth.png", bytes );
+	     		write_once = true;
+			}
+
+
 		}
 		else
 		{
