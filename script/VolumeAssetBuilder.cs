7b3777d551a104135f0894f43bc2e07774eab9ad
hyunxi <hyunxi@hotmail.com>
2020-04-11 12:42:32 -0700

    add volumn class add 3d 2d index converting

--- script/VolumeAssetBuilder.cs
+++ script/VolumeAssetBuilder.cs
@@ -7,6 +7,86 @@ using UnityEditor;
 
 namespace VolumeRendering
 {
+    public class d3_d2_volumn_info
+    {
+        //2dtexture width / height
+        public int d2_w;
+        public int d2_h;
+        public int d2_r;
+        public int d2_c;
+
+        //3dtexture w/h/depth
+        public int d3_w;
+        public int d3_h;
+        public int d3_d;
+
+        //d2 d3 coordinate structure
+        public struct d2_coord
+        {
+            public int x ;
+            public int y ;
+
+            public d2_coord(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        };
+
+        public struct d3_coord
+        {
+            public int x ;
+            public int y ;
+            public int z ;
+            public d3_coord(int _x, int _y, int _z)
+            {
+                x = _x;
+                y = _y;
+                z = _z;
+            }
+        };
+
+        public d3_d2_volumn_info (int d2_w, int d2_h, int d2_r, int d2_c)
+        {
+            this.d2_w = d2_w;
+            this.d2_h = d2_h;
+            this.d2_r = d2_r;
+            this.d2_c = d2_c;
+
+            this.d3_w = this.d2_w / this.d2_c;
+            this.d3_h = this.d2_h / this.d2_r;
+            this.d3_d = this.d2_c * this.d2_r;
+        }
+
+        public d3_d2_volumn_info (int d3_w, int d3_h, int d3_d, int d2_r, int d2_c)
+        {
+            this.d3_w = d3_w;
+            this.d3_h = d3_h;
+            this.d3_d = d3_d;
+
+            this.d2_r = d2_r;
+            this.d2_c = d2_c;
+
+            this.d2_w = this.d3_w * this.d2_c;
+            this.d2_h = this.d3_h * this.d2_r;
+        }
+
+        public void d3_to_d2_pix_cood(d3_coord d3c, ref d2_coord d2c)
+        {
+            if ((d3c.x > this.d3_w) ||(d3c.y > this.d3_h) || (d3c.z > this.d3_d))
+            {
+                d2c.x = -1;
+                d2c.y = -1;
+            }
+
+            int main_index = d3c.z * this.d3_w * this.d3_h + d3c.y * this.d3_w + d3c.x;
+            d2c.x = main_index % this.d2_w;
+            d2c.y = main_index / this.d2_w; 
+            // Debug.Log("main_index  :" + main_index );
+            // Debug.Log("d2c.x :" + d2c.x);
+            // Debug.Log("d2c.y :" + d2c.y);
+        }
+    }
 
     public class VolumeAssetBuilder : EditorWindow {
 
@@ -18,8 +98,8 @@ namespace VolumeRendering
         }
 
         string inputPath, outputPath;
-        int width = 256, height = 256, depth = 256;
-        Object asset;
+        int row = 12, column = 12;
+        Object source_texture;
 
         void OnEnable()
         {
@@ -27,25 +107,25 @@ namespace VolumeRendering
             outputPath = "Assets/T_Volume_Wisp_01.asset";
 
             //Load a Texture (Assets/Resources/Textures/texture01.png)
-            var texture = Resources.Load<Texture2D>("T_Volume_Wisp_01");
-            Debug.Log("texture load:" + texture);
-            if (texture == null)
-            {
-                Debug.Log("failed!");
+            // var texture = Resources.Load<Texture2D>("T_Volume_Wisp_01");
+            // Debug.Log("texture load:" + texture);
+            // if (texture == null)
+            // {
+            //     Debug.Log("failed!");
 
-            }
-            else
-            {
-                Debug.Log("OK");
-                for(int i = 0 ; i<texture.width; i++)
-                {
-                    for(int j = 0 ; j<texture.height; j++)
-                    {
-                        Color pixel = texture.GetPixel(i,j);
-                        Debug.Log("pixel =" + pixel);
-                    }   
-                }
-            }
+            // }
+            // else
+            // {
+            //     Debug.Log("OK");
+                // for(int i = 0 ; i<texture.width; i++)
+                // {
+                //     for(int j = 0 ; j<texture.height; j++)
+                //     {
+                //         Color pixel = texture.GetPixel(i,j);
+                //         Debug.Log("pixel =" + pixel);
+                //     }   
+                // }
+            //}
 
 
         }
@@ -56,28 +136,28 @@ namespace VolumeRendering
             
             using(new EditorGUILayout.HorizontalScope())
             {
-                GUILayout.Label("Input pvm raw file path", GUILayout.Width(headerSize));
-                asset = EditorGUILayout.ObjectField(asset, typeof(Object), true);
-                inputPath = AssetDatabase.GetAssetPath(asset);
+                GUILayout.Label("Input Image", GUILayout.Width(headerSize));
+                source_texture = EditorGUILayout.ObjectField(source_texture, typeof(Object), true);
+                inputPath = AssetDatabase.GetAssetPath(source_texture);
             }
 
-            using(new EditorGUILayout.HorizontalScope())
-            {
-                GUILayout.Label("Width", GUILayout.Width(headerSize));
-                width = EditorGUILayout.IntField(width);
-            }
+            // using(new EditorGUILayout.HorizontalScope())
+            // {
+            //     GUILayout.Label("Width", GUILayout.Width(headerSize));
+            //     width = EditorGUILayout.IntField(width);
+            // }
 
-            using(new EditorGUILayout.HorizontalScope())
-            {
-                GUILayout.Label("Height", GUILayout.Width(headerSize));
-                height = EditorGUILayout.IntField(height);
-            }
+            // using(new EditorGUILayout.HorizontalScope())
+            // {
+            //     GUILayout.Label("Height", GUILayout.Width(headerSize));
+            //     height = EditorGUILayout.IntField(height);
+            // }
 
-            using(new EditorGUILayout.HorizontalScope())
-            {
-                GUILayout.Label("Depth", GUILayout.Width(headerSize));
-                depth = EditorGUILayout.IntField(depth);
-            }
+            // using(new EditorGUILayout.HorizontalScope())
+            // {
+            //     GUILayout.Label("Depth", GUILayout.Width(headerSize));
+            //     depth = EditorGUILayout.IntField(depth);
+            // }
 
             using(new EditorGUILayout.HorizontalScope())
             {
@@ -87,65 +167,72 @@ namespace VolumeRendering
 
             if(GUILayout.Button("Build"))
             {
-                Build(inputPath, outputPath, width, height, depth);
+                Build(source_texture, outputPath);
             }
         }
 
-        void Build(
-            string inputPath, 
-            string outputPath, 
-            int width,
-            int height,
-            int depth 
-        )
+        void Build( Object myAsset, string outputPath)
         {
-            if (!File.Exists(inputPath))
+            if (myAsset == null)
             {
-                Debug.LogWarning(inputPath + " is not exist.");
+                Debug.LogWarning("input source texture is empty");
                 return;
             }
+            source_texture = EditorGUILayout.ObjectField(source_texture, typeof(Object), true);
+            Texture2D myTexture = (Texture2D)source_texture;
 
-            var volume = Build(inputPath, width, height, depth);
+            var volume = Build(myTexture, row,column);
             AssetDatabase.CreateAsset(volume, outputPath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
         }
 
-        public static Texture3D Build(string path, int width, int height, int depth)
+
+
+
+        public static Texture3D Build(Texture2D myTexture,int row, int column)
         {
-            Debug.LogWarning("inside creation");
-            var max = width * height * depth;
-            Debug.LogWarning("max = "+max);
-            var tex  = new Texture3D(width, height, depth, TextureFormat.ARGB32, false);
+            d3_d2_volumn_info v_info = new d3_d2_volumn_info (myTexture.width, myTexture.height, row, column);
+            Debug.Log("v_info.d3_w :" + v_info.d3_w);
+            Debug.Log("v_info.d3_h :" + v_info.d3_h);
+            Debug.Log("v_info.d3_d :" + v_info.d3_d);
+
+            var tex  = new Texture3D(v_info.d3_w, v_info.d3_h, v_info.d3_d, TextureFormat.RGBA32, false);
             tex.wrapMode = TextureWrapMode.Clamp;
             tex.filterMode = FilterMode.Bilinear;
             tex.anisoLevel = 0;
 
-            using(var stream = new FileStream(path, FileMode.Open))
-            {
-                var len = stream.Length;
-                Debug.LogWarning("len = "+len);
-                if(len != max)
-                {
-                    Debug.LogWarning(path + " doesn't have required resolution");
-                }
+            //int i = 0;
+            Color[] colors = new Color[v_info.d3_w * v_info.d3_h * v_info.d3_d];
+            float inv = 1f / 255.0f;
 
-                int i = 0;
-                Color[] colors = new Color[max];
-                float inv = 1f / 255.0f;
-                for(i = 0; i < stream.Length; i++)
+            for(int z = 0 ; z< v_info.d3_d; z++)
+            {
+                for(int y = 0 ; y< v_info.d3_h; y++)
                 {
-                    if(i == max)
+                    for(int x = 0 ; x< v_info.d3_w ; x++)
                     {
-                        break;
+                        //convert 3d to 3d coord
+                        d3_d2_volumn_info.d2_coord d2c = new d3_d2_volumn_info.d2_coord(-1,-1);
+                        d3_d2_volumn_info.d3_coord d3c = new d3_d2_volumn_info.d3_coord(x,y,z);
+
+                        v_info.d3_to_d2_pix_cood(d3c, ref d2c);                         
+                        
+                        // if (z == 15)
+                        // {
+                        //     Debug.Log("d2c : "+"x=" + d2c.x+ ", y=" + d2c.y);
+                        //     Debug.Log("d3c : "+"x=" + d3c.x+ ", y=" + d3c.y + ", z=" + d3c.z);
+                        // }
+                        
+                        colors[z* v_info.d3_w *v_info.d3_h + y * v_info.d3_w +x] = myTexture.GetPixel(d2c.x,d2c.y);
                     }
-                    int v = stream.ReadByte();
-                    float f = v * inv;
-                    colors[i] = new Color(f, f, f, f);
-                }
-                tex.SetPixels(colors);
-                tex.Apply();
+                }   
             }
+
+            tex.SetPixels(colors);
+            Debug.Log("colors");
+            tex.Apply();
+
             return tex;
         }
 
