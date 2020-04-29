a0e3c9b5ea64f4a023868d331ca729d080e1c47c
hyunxi <hyunxi@hotmail.com>
2020-04-29 12:13:19 -0700

    add Dcube Dcube pool wip

--- script/intersector.cs
+++ script/intersector.cs
@@ -30,6 +30,65 @@ class AABB
     }
 }
 
+class DCube_pool
+{
+    private List <DCube> _pool;
+    public DCube_pool ()
+    {
+        this._pool =  new List<DCube>();
+    }   
+    DCube getDCube()
+    {
+        //check list for unused Dcube
+        //get one unused Dcube and reutnr
+    }
+    void killDcube()
+    {
+        //set the reutnr Dcube as unused
+    }
+}
+
+class DCube
+{
+    private GameObject _cube;
+    private bool _used;
+    public DCube(string n, float s, Vector3 p, Color c)
+    {
+        this._cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        this._cube.transform.position = p;
+        this._cube.transform.localScale = new Vector3(s,s,s);
+        this._cube.GetComponent<MeshRenderer>().material.color = c;
+        this._cube.GetComponent<MeshRenderer>().enabled = false;
+        this._cube.name = n;
+    }
+    public GameObject cube
+    {
+        get{return this._cube;}
+    }
+    public bool used_state
+    {
+        get{return this._used;}
+    }
+    public void use()
+    {
+        this.show( true);
+        this._used = true;
+    }
+    public void release()
+    {
+        this.show(false);
+        this._used = false;
+    }
+    public void show(bool show)
+    {
+        this._cube.GetComponent<MeshRenderer>().enabled = show;
+    }
+    public void setParent (Transform p)
+    {
+        this._cube.transform.parent = p;
+    }
+}
+
 class OBB
 {
     private GameObject cube;
@@ -131,12 +190,20 @@ class AB_RAY
         this.PA = A;
         this.PB = B;
     }
+    public Vector3 fullRay
+    {
+        get
+        { 
+            return  PB.position - PA.position;
+        }
+    }
 }
 
 public class intersector : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject test_cube;
+    List <GameObject> debugCubes;
     public bool is_aabb;
     public int max_steps = 100;
     Camera cam;
@@ -154,6 +221,16 @@ public class intersector : MonoBehaviour
 
     void create_cube(ref GameObject _cube, string _name , Vector3 _pos, float _size, Color _col)
     {
+        _cube.transform.position = _pos;
+        _cube.transform.localScale = new Vector3(_size,_size,_size);
+        _cube.GetComponent<MeshRenderer>().material.color = _col;
+        _cube.GetComponent<MeshRenderer>().enabled = false;
+        _cube.name = _name;
+    }
+
+    void create_cube_in(out GameObject _cube, string _name , Vector3 _pos, float _size, Color _col)
+    {
+        _cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         _cube.transform.position = _pos;
         _cube.transform.localScale = new Vector3(_size,_size,_size);
         _cube.GetComponent<MeshRenderer>().material.color = _col;
@@ -209,18 +286,17 @@ public class intersector : MonoBehaviour
 
     void display_ref_cube(AB_RAY _ray, Vector3 xyz_sclae_min , Vector3 xyz_sclae_max, bool _display_ref ,  bool _display_int1, bool _display_int2)
     {
-        Vector3 ray_full = _ray.PB.position -_ray.PA.position;
-        cube_x1.transform.position = _ray.PA.position + xyz_sclae_min.x * ray_full;
-        cube_x2.transform.position = _ray.PA.position + xyz_sclae_max.x * ray_full;
+        cube_x1.transform.position = _ray.PA.position + xyz_sclae_min.x * _ray.fullRay;
+        cube_x2.transform.position = _ray.PA.position + xyz_sclae_max.x * _ray.fullRay;
 
-        cube_y1.transform.position = _ray.PA.position + xyz_sclae_min.y * ray_full;
-        cube_y2.transform.position = _ray.PA.position + xyz_sclae_max.y * ray_full;
+        cube_y1.transform.position = _ray.PA.position + xyz_sclae_min.y * _ray.fullRay;
+        cube_y2.transform.position = _ray.PA.position + xyz_sclae_max.y * _ray.fullRay;
 
-        cube_z1.transform.position = _ray.PA.position + xyz_sclae_min.z * ray_full;
-        cube_z2.transform.position = _ray.PA.position + xyz_sclae_max.z * ray_full;
+        cube_z1.transform.position = _ray.PA.position + xyz_sclae_min.z * _ray.fullRay;
+        cube_z2.transform.position = _ray.PA.position + xyz_sclae_max.z * _ray.fullRay;
 
-        cube_in1.transform.position =_ray.PA.position +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *ray_full;
-        cube_in2.transform.position =_ray.PA.position +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *ray_full;
+        cube_in1.transform.position =_ray.PA.position +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *_ray.fullRay;
+        cube_in2.transform.position =_ray.PA.position +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *_ray.fullRay;
 
         cube_x1.GetComponent<Renderer>().enabled = _display_ref;
         cube_x2.GetComponent<Renderer>().enabled = _display_ref;
@@ -234,41 +310,40 @@ public class intersector : MonoBehaviour
     
     void aabb_intersection(AB_RAY _ray, AABB _box)
     {
-        Vector3 ray_full = _ray.PB.position -_ray.PA.position;
         Vector3 min_inter , max_inter; // intersection point
         bool min_exist, max_exist;
 
         Vector3 ray_min = _box.min - _ray.PA.position;
         Vector3 ray_max = _box.max - _ray.PA.position;
 
-        float scale_min = ray_min.magnitude / ray_full.magnitude;
-        float scale_max = ray_max.magnitude / ray_full.magnitude;
+        float scale_min = ray_min.magnitude / _ray.fullRay.magnitude;
+        float scale_max = ray_max.magnitude / _ray.fullRay.magnitude;
 
         Vector3 xyz_sclae_min = new Vector3 (scale_min , scale_min, scale_min);
         Vector3 xyz_sclae_max = new Vector3 (scale_max , scale_max, scale_max);
 
-         if (ray_full.x != 0)
+         if (_ray.fullRay.x != 0)
          {
-            float _x1 = ray_min.x / ray_full.x;
-            float _x2 =  ray_max.x / ray_full.x;
+            float _x1 = ray_min.x / _ray.fullRay.x;
+            float _x2 =  ray_max.x / _ray.fullRay.x;
 
             xyz_sclae_min.x = _x1 < _x2 ? _x1 : _x2;
             xyz_sclae_max.x =  _x1 > _x2 ? _x1 : _x2;
          }
 
-         if (ray_full.y != 0)
+         if (_ray.fullRay.y != 0)
          {
-            float _y1 = ray_min.y / ray_full.y;
-            float _y2 =  ray_max.y / ray_full.y;
+            float _y1 = ray_min.y / _ray.fullRay.y;
+            float _y2 =  ray_max.y / _ray.fullRay.y;
 
             xyz_sclae_min.y = _y1 < _y2 ? _y1 : _y2;
             xyz_sclae_max.y =  _y1 > _y2 ? _y1 : _y2;
          }
 
-        if (ray_full.z != 0)
+        if (_ray.fullRay.z != 0)
         {
-           float _z1 = ray_min.z / ray_full.z;
-           float _z2 =  ray_max.z / ray_full.z;
+           float _z1 = ray_min.z / _ray.fullRay.z;
+           float _z2 =  ray_max.z / _ray.fullRay.z;
            xyz_sclae_min.z = _z1 < _z2 ? _z1 : _z2;
            xyz_sclae_max.z =  _z1 > _z2 ? _z1 : _z2;
         }
@@ -276,8 +351,8 @@ public class intersector : MonoBehaviour
         float min_scale = Mathf.Max(Mathf.Max(xyz_sclae_min.x ,xyz_sclae_min.y),xyz_sclae_min.z);
         float max_scale = Mathf.Min(Mathf.Min(xyz_sclae_max.x ,xyz_sclae_max.y),xyz_sclae_max.z);
 
-        min_inter =_ray.PA.position +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *ray_full;
-        max_inter =_ray.PA.position +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *ray_full;
+        min_inter =_ray.PA.position +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *_ray.fullRay;
+        max_inter =_ray.PA.position +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *_ray.fullRay;
 
         min_exist=false;
         max_exist=false;
@@ -303,7 +378,6 @@ public class intersector : MonoBehaviour
 
     void obb_intersection(AB_RAY _ray, OBB _box)
     {
-        Vector3 ray_full = _ray.PB.position -_ray.PA.position;
         Vector3 min_inter , max_inter; // intersection point
         bool min_exist, max_exist;
 
@@ -314,7 +388,7 @@ public class intersector : MonoBehaviour
         Vector3 xyz_sclae_max;
 
         //full ray on x , y , z value
-        Vector3 ray_projected = new Vector3( Vector3.Dot(_box.x_axis ,ray_full) , Vector3.Dot(_box.y_axis ,ray_full) , Vector3.Dot(_box.z_axis ,ray_full));
+        Vector3 ray_projected = new Vector3( Vector3.Dot(_box.x_axis ,_ray.fullRay) , Vector3.Dot(_box.y_axis ,_ray.fullRay) , Vector3.Dot(_box.z_axis ,_ray.fullRay));
         ray_projected.x = ray_projected.x == 0?0.0000001f : ray_projected.x;
         ray_projected.y = ray_projected.y == 0?0.0000001f : ray_projected.y;
         ray_projected.z = ray_projected.z == 0?0.0000001f : ray_projected.z;
@@ -359,13 +433,12 @@ public class intersector : MonoBehaviour
         display_ref_cube(_ray, xyz_sclae_min , xyz_sclae_max, false, min_exist, max_exist);
     }
 
-    void obb_intersection_cube(AB_RAY _ray, OBB _box)
+    void obb_intersection_cube(AB_RAY _ray, OBB _box, out Vector2 s0 , out Vector2 s1)
     {
         Vector3 _PA_pos_cube = _box.w2o.MultiplyPoint3x4(_ray.PA.position);
         Vector3 _PB_pos_cube = _box.w2o.MultiplyPoint3x4(_ray.PB.position);
 
         //object space
-        Vector3 ray_full = _PB_pos_cube -_PA_pos_cube;
         Vector3 p0 , p1; // intersection point
         bool min_exist, max_exist;
 
@@ -378,25 +451,27 @@ public class intersector : MonoBehaviour
         Vector3 xyz_sclae_min;
         Vector3 xyz_sclae_max;
 
-        //full ray on x , y , z value
-        ray_full.x = ray_full.x == 0?0.0000001f : ray_full.x;
-        ray_full.y = ray_full.y == 0?0.0000001f : ray_full.y;
-        ray_full.z = ray_full.z == 0?0.0000001f : ray_full.z;
+        Vector3 fullRay = _ray.fullRay;
 
-        float _x1 = ray_min.x / ray_full.x;
-        float _x2 = ray_max.x / ray_full.x;
+        //full ray on x , y , z value
+        fullRay.x = fullRay.x == 0?0.0000001f : fullRay.x;
+        fullRay.y = fullRay.y == 0?0.0000001f : fullRay.y;
+        fullRay.z = fullRay.z == 0?0.0000001f : fullRay.z;
+
+        float _x1 = ray_min.x / fullRay.x;
+        float _x2 = ray_max.x / fullRay.x;
 
         xyz_sclae_min.x = _x1 < _x2 ? _x1 : _x2;
         xyz_sclae_max.x = _x1 > _x2 ? _x1 : _x2;
 
-        float _y1 = ray_min.y / ray_full.y;
-        float _y2 = ray_max.y / ray_full.y;
+        float _y1 = ray_min.y / fullRay.y;
+        float _y2 = ray_max.y / fullRay.y;
 
         xyz_sclae_min.y = _y1 < _y2 ? _y1 : _y2;
         xyz_sclae_max.y = _y1 > _y2 ? _y1 : _y2;
 
-        float _z1 = ray_min.z / ray_full.z;
-        float _z2 = ray_max.z / ray_full.z;
+        float _z1 = ray_min.z / fullRay.z;
+        float _z2 = ray_max.z / fullRay.z;
 
         xyz_sclae_min.z = _z1 < _z2 ? _z1 : _z2;
         xyz_sclae_max.z = _z1 > _z2 ? _z1 : _z2;
@@ -404,11 +479,14 @@ public class intersector : MonoBehaviour
         float min_scale = Mathf.Max(Mathf.Max(xyz_sclae_min.x ,xyz_sclae_min.y),xyz_sclae_min.z);
         float max_scale = Mathf.Min(Mathf.Min(xyz_sclae_max.x ,xyz_sclae_max.y),xyz_sclae_max.z);
 
-        //two intersect point in object space
-        p0 =_PA_pos_cube +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *ray_full;
-        p1 =_PA_pos_cube +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *ray_full;
+        s0.x = min_scale;
+        s1.x = max_scale;
+
+        //two intersect point in object space , this will need for ray marching
+        p0 =_PA_pos_cube +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *fullRay;
+        p1 =_PA_pos_cube +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *fullRay;
          
-         //position in object space
+         //position in world space
          p0 = _box.o2w.MultiplyPoint3x4(p0);
          p1 = _box.o2w.MultiplyPoint3x4(p1);
 
@@ -428,11 +506,17 @@ public class intersector : MonoBehaviour
             }
         }
 
-        display_ref_cube(_ray, xyz_sclae_min , xyz_sclae_max, false, min_exist, max_exist);
+        s0.y = min_exist ? 1:0;
+        s1.y = max_exist ? 1:0;
+
+        //debug function for displaying all the cubes on x y z bound, only support one ray
+        //display_ref_cube(_ray, xyz_sclae_min , xyz_sclae_max, false, min_exist, max_exist);
 
         // cube_in1.transform.position =p0;
         // cube_in2.transform.position =p1;
+
     }
+
 
 
     void Start()
@@ -446,27 +530,32 @@ public class intersector : MonoBehaviour
         h_rays = (int)((float)w_rays * cam.pixelHeight / cam.pixelWidth );
 
         cam_rays = new List<AB_RAY>();
+        debugCubes = new List<GameObject>();
         for (int i = 0; i <h_rays ;i++)
         {
             //need to create the last ray at right edge
             for (int j = 0; j <w_rays ;j++)
             {
+                //create ray at correct gap
                 Ray myRay = cam.ViewportPointToRay(new Vector3(1.0f/(w_rays-1)*j, 1.0f/(h_rays-1)*i, 0));
-                float _z = myRay.direction.z == 0 ? 0.000001f : myRay.direction.z;
 
                 Vector3 B = (myRay.origin - cam.transform.position)* cam.farClipPlane / cam.nearClipPlane + cam.transform.position;
 
-                GameObject f_cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                create_cube(ref f_cube, "test_far" , B, cube_size, new Color(0.5f,0f,0f));
-                f_cube.GetComponent<MeshRenderer>().enabled = true;
-                f_cube.transform.parent = cam.transform;
+                GameObject f_cube ;
+                DCube fc = new DCube("test_far", cube_size, B, new Color(0.5f,0f,0f));
+                fc.use(true);
+                fc.setParent(cam.transform);
+
+                //create_cube_in(out f_cube, "test_far" , B, cube_size, new Color(0.5f,0f,0f));
+                // f_cube.GetComponent<MeshRenderer>().enabled = true;
+                //f_cube.transform.parent = cam.transform;
 
                 GameObject n_cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 create_cube(ref n_cube, "near_far" , myRay.origin, cube_size, new Color(0.5f,0f,0f));  
                 n_cube.GetComponent<MeshRenderer>().enabled = true;
                 n_cube.transform.parent = cam.transform;
 
-                AB_RAY cam_ab_ray = new AB_RAY(cam.transform, f_cube.transform);
+                AB_RAY cam_ab_ray = new AB_RAY(n_cube.transform, fc.cube.transform);
                 cam_rays.Add(cam_ab_ray);
 
             }   
@@ -496,6 +585,33 @@ public class intersector : MonoBehaviour
         create_test_cubes();
     }
 
+    void debug_cube(AB_RAY _ray,float s)
+    {
+        GameObject cube_x1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+// void display_ref_cube(AB_RAY _ray, Vector3 xyz_sclae_min , Vector3 xyz_sclae_max, bool _display_ref ,  bool _display_int1, bool _display_int2)
+//     {
+//         cube_x1.transform.position = _ray.PA.position + xyz_sclae_min.x * _ray.fullRay;
+//         cube_x2.transform.position = _ray.PA.position + xyz_sclae_max.x * _ray.fullRay;
+
+//         cube_y1.transform.position = _ray.PA.position + xyz_sclae_min.y * _ray.fullRay;
+//         cube_y2.transform.position = _ray.PA.position + xyz_sclae_max.y * _ray.fullRay;
+
+//         cube_z1.transform.position = _ray.PA.position + xyz_sclae_min.z * _ray.fullRay;
+//         cube_z2.transform.position = _ray.PA.position + xyz_sclae_max.z * _ray.fullRay;
+
+//         cube_in1.transform.position =_ray.PA.position +   Mathf.Max(Mathf.Max(xyz_sclae_min.x  ,xyz_sclae_min.y),xyz_sclae_min.z) *_ray.fullRay;
+//         cube_in2.transform.position =_ray.PA.position +   Mathf.Min(Mathf.Min(xyz_sclae_max.x  ,xyz_sclae_max.y),xyz_sclae_max.z) *_ray.fullRay;
+
+//         cube_x1.GetComponent<Renderer>().enabled = _display_ref;
+//         cube_x2.GetComponent<Renderer>().enabled = _display_ref;
+//         cube_y1.GetComponent<Renderer>().enabled = _display_ref; 
+//         cube_y2.GetComponent<Renderer>().enabled = _display_ref; 
+//         cube_z1.GetComponent<Renderer>().enabled = _display_ref; 
+//         cube_z2.GetComponent<Renderer>().enabled = _display_ref; 
+//         cube_in1.GetComponent<Renderer>().enabled = _display_int1;
+//         cube_in2.GetComponent<Renderer>().enabled = _display_int2;
+//     }
     // Update is called once per frame
     void Update()
     {
@@ -507,7 +623,8 @@ public class intersector : MonoBehaviour
             for (int j = 0; j <w_rays ;j++)
             {
                 AB_RAY _ray = cam_rays[i *w_rays + j];
-                Debug.DrawLine(_ray.PA.position, _ray.PB.position, Color.white);
+                // Debug.DrawLine(_ray.PA.position, _ray.PB.position, Color.gray);
+                // obb_intersection_cube(_ray , obb);
             }   
         }
         
@@ -519,7 +636,9 @@ public class intersector : MonoBehaviour
         else
         {
             // obb_intersection(ab_ray , obb);
-            obb_intersection_cube(ab_ray , obb);
+            Vector2 s0;
+            Vector2 s1;
+            obb_intersection_cube(ab_ray , obb , out s0 , out s1);
             // Debug.DrawLine(obb.pos, obb.pos + obb.x_axis , Color.red);
             // Debug.DrawLine(obb.pos, obb.pos + obb.y_axis , Color.green);
             // Debug.DrawLine(obb.pos, obb.pos + obb.z_axis , Color.blue);
